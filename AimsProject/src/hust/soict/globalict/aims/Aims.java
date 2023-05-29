5847cb9c065e12a55399f6c68871a40c252a328e
Pert913 <tuemun13092003@gmail.com>
2023-05-30 00:09:45 +0700

    .

--- AimsProject/src/hust/soict/globalict/aims/Aims.java
+++ AimsProject/src/hust/soict/globalict/aims/Aims.java
@@ -63,8 +63,8 @@ public class Aims {
     public static void filterCart() {
         System.out.println("\nOptions: "); 
 		System.out.println("--------------------------------"); 
-        System.out.println("1. Filter by title");
-        System.out.println("2. Filter by cost");
+        System.out.println("1. Filter by id");
+        System.out.println("2. Filter by title");
         System.out.println("0. Back");
         System.out.println("--------------------------------"); 
         System.out.println("Please choose a number: 0-1-2"); 
@@ -73,8 +73,8 @@ public class Aims {
     public static void sortCart() {
         System.out.println("\nOptions: "); 
 		System.out.println("--------------------------------"); 
-        System.out.println("1. Sort by id");
-        System.out.println("2. Sort by title");
+        System.out.println("1. Sort by title");
+        System.out.println("2. Sort by cost");
         System.out.println("0. Back");
         System.out.println("--------------------------------"); 
         System.out.println("Please choose a number: 0-1-2"); 
